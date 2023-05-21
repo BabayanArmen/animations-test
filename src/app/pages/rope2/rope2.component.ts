cae68531bd04f0f4bc17e8f4824e708ceb55a230
Armen Babayan <armenbabayan1984@gmail.com>
2023-05-21 18:54:32 +0400

    changes

--- src/app/pages/rope2/rope2.component.ts
+++ src/app/pages/rope2/rope2.component.ts
@@ -7,8 +7,12 @@ import { AfterViewInit, Component, ElementRef, HostListener, Inject, OnInit, PLA
   styleUrls: ['./rope2.component.scss']
 })
 export class Rope2Component implements OnInit, AfterViewInit {
-  @ViewChild("rope") ropeEl!: ElementRef;
-  @ViewChild("ropeWrapper") ropeWrapperEl!: ElementRef;
+  @ViewChild("rope") rope!: ElementRef;
+  @ViewChild("ropeWrapper") ropeWrapper!: ElementRef;
+  public ropes:string [] = ["rope"];
+
+  public showButton2:boolean = false;
+  public showButton3:boolean = false;
 
   constructor(
     @Inject(DOCUMENT) private document: Document,
@@ -18,17 +22,28 @@ export class Rope2Component implements OnInit, AfterViewInit {
   ngOnInit(): void { }
 
   ngAfterViewInit(): void {
-    if (isPlatformBrowser(this.platformId)) {
-      // this.ropeEl.nativeElement.style.height = this.document.body.offsetHeight + "px";
-    }
+    // if (isPlatformBrowser(this.platformId)) {
+    //   // this.rope.nativeElement.style.height = this.document.body.offsetHeight + "px";
+    // }
   }
 
   @HostListener('window:scroll', ['$event'])
   onScroll() {
     if (isPlatformBrowser(this.platformId)) {
       let scrolledSize = window.scrollY  // document.documentElement.scrollTop
+      let viewportHeight = document.documentElement.clientHeight // window.innerHeight
+      let documentHeight = document.body.offsetHeight;
+  
+      let scrollbarThumbRatio = viewportHeight / documentHeight;
+      let scrollbarThumbSize = scrollbarThumbRatio * viewportHeight;
+  
       setTimeout(() => {
-        this.ropeWrapperEl.nativeElement.style.height = this.document.documentElement.clientHeight + scrolledSize + "px";
+        if(scrolledSize + viewportHeight + scrollbarThumbSize > 7199) this.ropes.push("rope");
+        this.ropeWrapper.nativeElement.style.height = this.document.documentElement.clientHeight + scrolledSize + "px";
+
+        this.showButton2 = scrolledSize + viewportHeight + scrollbarThumbSize > 765; 
+        this.showButton3 = scrolledSize + viewportHeight + scrollbarThumbSize > 1500;
+        
       }, 500)
     }
   }
